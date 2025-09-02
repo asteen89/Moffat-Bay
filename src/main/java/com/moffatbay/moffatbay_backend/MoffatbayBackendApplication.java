cae02d3a4aaf962bee550d8363ac7d9d79e16882
Carmen Mendoza <carmenmendoza@Carmens-MacBook-Air.local>
2025-09-01 22:27:20 -0400

    Initial backend setup: add HomeApi, application.properties, and project files

---
+++ src/main/java/com/moffatbay/moffatbay_backend/MoffatbayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moffatbay.moffatbay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoffatbayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoffatbayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moffatbay/moffatbay_backend/MoffatbayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moffatbay.moffatbay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoffatbayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
