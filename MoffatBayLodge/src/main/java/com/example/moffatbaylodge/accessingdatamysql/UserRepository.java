4d46b9781a56b143b622cb4edcbc8de2a82003cb
Jose Franco <jofranco@my365.bellevue.edu>
2025-09-20 23:59:43 -0500

    Look-up reservation backend

--- MoffatBayLodge/src/main/java/com/example/moffatbaylodge/accessingdatamysql/UserRepository.java
+++ MoffatBayLodge/src/main/java/com/example/moffatbaylodge/accessingdatamysql/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmailAddress(String emailAddress);
+    Optional<User> findByEmailAddressIgnoreCase(String emailAddress);
 }
